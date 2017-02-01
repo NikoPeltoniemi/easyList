deac76f8e951cbe00bf7be32b370a2073abe9f2d
Niko Peltoniemi <niko@potentialfuturenonsense.com>
2017-02-01 14:22:03 +0200

    Added custom color option for groups

--- easyList.php
+++ easyList.php
@@ -46,6 +46,49 @@ function pfn_ukk_setup_post_types(){
 }
 add_action( 'init', 'sst_ukk_setup_post_types');
 
+function pfn_taxonomy_add_new_meta_field(){
+	?>
+	<div class="form-field">
+		<label for="term_meta[ukk_heading_color]"><?php _e('Color override (Hex)', 'pfn-ukk'); ?></label>
+		<input type="text" name="term_meta[ukk_heading_color]" value="">
+		<p class="description"><?php _e('Enter custom color for question bars', 'pfn-ukk'); ?></p>
+	</div>
+<?php
+}
+add_action('ukk-heading_add_form_fields', 'pfn_taxonomy_add_new_meta_field');
+
+function pfn_taxonomy_edit_meta_field($term){
+	$t_id = $term->term_id;
+
+	$term_meta = get_option("taxonomy_$t_id");?>
+	<tr class="form-field">
+	<th scope="row" valign="top"><label for="term_meta[ukk_heading_color]"><?php _e('Color override (Hex', 'pfn-ukk'); ?></label></th>
+		<td>
+			<input type="text" name="term_meta[ukk_heading_color]" id="term_meta[ukk_heading_color]" value="<?php echo esc_attr($term_meta['ukk_heading_color']) ? esc_attr($term_meta['ukk_heading_color']) : ''; ?>">
+			<p class="description"><?php _e('Enter custom color for question bars', 'pfn-ukk'); ?></p>
+		</td>
+	</tr>
+<?php
+}
+add_action('ukk-heading_edit_form_fields', 'pfn_taxonomy_edit_meta_field');
+
+function save_taxonomy_custom_meta($term_id){
+	if(isset($_POST['term_meta'])){
+		$t_id = $term_id;
+		$term_meta = get_option("taxonomy_$t_id");
+		$cat_keys = array_keys($_POST['term_meta']);
+		foreach($cat_keys as $key){
+			if(isset($_POST['term_meta'][$key])){
+				$term_meta[$key] = $_POST['term_meta'][$key];
+			}
+		}
+
+		update_option( "taxonomy_$t_id", $term_meta );
+	}
+}
+add_action('edited_ukk-heading', 'save_taxonomy_custom_meta');
+add_action('create_ukk-heading', 'save_taxonomy_custom_meta');
+
 function pfn_ukk_install(){
 
 	pfn_ukk_setup_post_types();
@@ -97,7 +140,12 @@ function pfn_ukk_shortcode($atts){
 			while($query->have_posts()){
 				$query->the_post();
 				if(has_term($atts['group'], 'ukk-heading')){
-					$html .= '<div class="ukk">';
+					if(get_term_meta(intval(get_term_by('name','ukk-heading')), 'ukk_heading_color')){
+						$color = get_term_meta(intval(get_term_by('name','ukk-heading')), 'ukk_heading_color');
+						$html .= '<div class="ukk" style="background-color: '.$color.';">';
+					}else{
+						$html .= '<div class="ukk">';
+					}
 					$html .= '<a class="ukk-head" href="#'. get_the_title().'">'. get_the_title().'</a>';
 					$html .= '<div class="ukk-answer">';
 					$html .= get_the_content();
